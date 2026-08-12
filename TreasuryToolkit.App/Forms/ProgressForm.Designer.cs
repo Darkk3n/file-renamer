56c46b10ab70657d6288c23003dead6565576978
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-08-12 16:48:49 -0600

    Changed progress form to display a progress bar

--- TreasuryToolkit.App/Forms/ProgressForm.Designer.cs
+++ TreasuryToolkit.App/Forms/ProgressForm.Designer.cs
@@ -28,53 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressForm));
-            pictureBox1 = new PictureBox();
             LblStatus = new Label();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            progressBar1 = new ProgressBar();
             SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Dock = DockStyle.Top;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(536, 406);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
             // 
             // LblStatus
             // 
             LblStatus.AutoSize = true;
             LblStatus.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            LblStatus.Location = new Point(44, 419);
+            LblStatus.Location = new Point(21, 9);
             LblStatus.Name = "LblStatus";
             LblStatus.Size = new Size(169, 25);
             LblStatus.TabIndex = 1;
             LblStatus.Text = "Procesando PDFs...";
             // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(21, 47);
+            progressBar1.MarqueeAnimationSpeed = 50;
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(548, 23);
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.TabIndex = 2;
+            progressBar1.Click += progressBar1_Click;
+            // 
             // ProgressForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(536, 453);
+            BackColor = SystemColors.ControlLightLight;
+            ClientSize = new Size(581, 84);
+            Controls.Add(progressBar1);
             Controls.Add(LblStatus);
-            Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "ProgressForm";
             ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterParent;
             Text = "ProgressForm";
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private Label LblStatus;
+        private ProgressBar progressBar1;
     }
 }
