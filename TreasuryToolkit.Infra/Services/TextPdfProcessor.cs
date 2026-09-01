afc0579d3fa114979b04aef1bbe624944229e757
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-08-31 19:52:50 -0600

    Added dash between vendor and concept

--- TreasuryToolkit.Infra/Services/TextPdfProcessor.cs
+++ TreasuryToolkit.Infra/Services/TextPdfProcessor.cs
@@ -39,7 +39,7 @@ namespace TreasuryToolkit.Infra.Services
                 var comment = string.Join("_", (row.Comment ?? string.Empty).Split(Path.GetInvalidFileNameChars())).Trim();
 
                 string directory = Path.GetDirectoryName(currentFilePath);
-                string newFileName = $"{row.Date}-{companyName}-{vendor} {concept}-{amount} {currency}";
+                string newFileName = $"{row.Date}-{companyName}-{vendor}-{concept}-{amount} {currency}";
                 if (comment.HasValue)
                 {
                     newFileName+=$" ({comment})";
