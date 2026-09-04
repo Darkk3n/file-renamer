5df736d85b3418005e327b877de585889a57f6ab
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2026-09-04 16:10:57 -0600

    Added new extensions file

---
+++ TreasuryToolkit.Core/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TreasuryToolkit.Core;
+
+public static class Extensions
+{
+    public static bool In<TSource>(this TSource source, params TSource[] list)
+        => list.Contains(source);
+
+    public static bool In<TSource>(this TSource source, IEnumerable<TSource> collection)
+        => collection.Contains(source);
+}
